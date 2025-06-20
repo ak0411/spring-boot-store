0933618fa271b9578155e20afaa291db9c137017
Anawil Karavek <akaravek@hotmail.com>
2025-06-20 14:07:12 +0200

    fist commit

---
+++ src/test/java/com/akaravek/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akaravek.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
